0aa5b3159302db7a81a023fe1801663d74e84cf9
Marouane EL MERROUNI <melmourrouni-externe@sigma.fr>
2021-10-28 00:02:32 +0100

    [TECH] Runnunin springBoot in embeded tomcat

---
+++ src/main/java/ma/norsys/pocscheduler/ServletInitializer.java
@@ -0,0 +1,14 @@
+package ma.norsys.pocscheduler;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PocschedulerApplication.class);
+    }
+
+}
+
