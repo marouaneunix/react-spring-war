a7e39afa3d4a964492cfeb1279807a489af198ee
Marouane EL MERROUNI <melmourrouni-externe@sigma.fr>
2021-10-27 21:42:15 +0100

    [TECH] POC Multi datasources OK :fire:

---
+++ src/main/java/ma/norsys/pocscheduler/configuration/PersistencePccConfiguration.java
@@ -0,0 +1,69 @@
+package ma.norsys.pocscheduler.configuration;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "ma.norsys.pocscheduler.repository",
+        entityManagerFactoryRef = "pccEntityManager",
+        transactionManagerRef = "pccTransactionManager"
+)
+public class PersistencePccConfiguration {
+
+    private final Environment env;
+
+    public PersistencePccConfiguration(Environment env) {
+        this.env = env;
+    }
+
+    @Primary
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource pccDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean pccEntityManager() {
+        LocalContainerEntityManagerFactoryBean em
+                = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(pccDataSource());
+        em.setPackagesToScan(
+                new String[]{"ma.norsys.pocscheduler.repository"});
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        em.setJpaPropertyMap(properties);
+
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager pccTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(pccEntityManager().getObject());
+        return transactionManager;
+    }
+
+    @Bean
+    @FlywayDataSource
+    public DataSource flywayDataSource() {
+        return pccDataSource();
+    }
+}
--- src/main/java/ma/norsys/pocscheduler/service/BatchService.java
+++ src/main/java/ma/norsys/pocscheduler/service/BatchService.java
@@ -1,12 +1,21 @@
 package ma.norsys.pocscheduler.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import ma.norsys.pocscheduler.repository.Task;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class BatchService{
 
+    private final JdbcTemplate jdbcTemplate;
     public void runBatch(Task task) {
-        System.out.println("Running batch of task "+ task.getName());
+        jdbcTemplate.query("select * from dwh",rs -> {
+            log.info("dwh id {}, name {}",rs.getLong("id"),rs.getString("name"));
+        });
+        log.info("Running batch of task {}",task.getName());
     }
 }
