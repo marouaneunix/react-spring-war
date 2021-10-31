d6aaf0cb134abddb15fe7083665980a285fff21f
Marouane EL MERROUNI <melmourrouni-externe@sigma.fr>
2021-10-31 22:43:31 +0100

    [TECH] react router tomcat OK

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,59 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import {HashRouter, Link, Route, RouteComponentProps} from "react-router-dom";
 
 interface Task {
-  name:string,
-  cron:string
+    name: string,
+    cron: string
+}
+
+
+const Index = () => <h2>Home</h2>;
+
+const Tasks = ({match}: RouteComponentProps<{ id: string }>) => {
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    useEffect(() => {
+        const getTasks = async () => {
+            const fetchedTasks = await axios.get('/api/tasks');
+            setTasks(fetchedTasks.data);
+        };
+        getTasks();
+    }, []);
+    return (
+        <>
+            <h2>This is a page for tasks with ID: {match.params.id} </h2>
+            {
+                tasks.map(task => <p>{task.name} {task.cron}</p>)
+            }
+        </>
+    )
 }
 
 function App() {
-  const [tasks, setTasks] = useState<Task[]>([]);
 
-  useEffect(() => {
-    const getTasks = async () => {
-      const fetchedTasks = await axios.get('/api/tasks');
-      setTasks(fetchedTasks.data);
-    };
-    getTasks();
-  }, []);
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          {
-            tasks.map(task => <p>{task.name} {task.cron}</p>)
-          }
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <HashRouter>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/tasks/1">First Task</Link>
+                        </li>
+                        <li>
+                            <Link to="/tasks/2">Second Task</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Route path="/" exact component={Index}/>
+                <Route path="/tasks/:id" component={Tasks}/>
+            </div>
+        </HashRouter>
+    );
 }
 
 export default App;
