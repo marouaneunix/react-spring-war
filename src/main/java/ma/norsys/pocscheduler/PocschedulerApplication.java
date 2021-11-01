0a34b0852fc5995cf02c45be99f6f56449837543
Marouane EL MERROUNI <melmourrouni-externe@sigma.fr>
2021-11-01 13:01:11 +0100

    [TECH] corss error ok

--- src/main/java/ma/norsys/pocscheduler/PocschedulerApplication.java
+++ src/main/java/ma/norsys/pocscheduler/PocschedulerApplication.java
@@ -6,13 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.security.Principal;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -32,7 +28,8 @@ public class PocschedulerApplication {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				.select()                 .apis(RequestHandlerSelectors.basePackage("ma.norsys.pocscheduler"))
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("ma.norsys.pocscheduler"))
 				.paths(regex("/api.*"))
 				.build();
 
