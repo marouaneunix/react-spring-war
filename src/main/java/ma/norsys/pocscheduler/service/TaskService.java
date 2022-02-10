acb750a34bd6ec568213c669b71cb7c8fa66253f
marouaneunix <elmerrouni.marouane@gmail.com>
2022-02-10 18:09:52 +0100

    JAR

--- src/main/java/ma/norsys/pocscheduler/service/TaskService.java
+++ src/main/java/ma/norsys/pocscheduler/service/TaskService.java
@@ -2,38 +2,17 @@ package ma.norsys.pocscheduler.service;
 
 
 import lombok.RequiredArgsConstructor;
-import ma.norsys.pocscheduler.repository.Task;
-import ma.norsys.pocscheduler.repository.TaskRepository;
+import ma.norsys.pocscheduler.controller.TaskDto;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class TaskService {
 
-    private final TaskRepository taskRepository;
-    private final SchedulerService schedulerService;
 
-    public void save(Task task) {
-        this.taskRepository.save(task);
-        schedulerService.schedule(task);
-    }
-
-    public void enableTask(Long taskId) {
-        Task task = this.taskRepository.findById(taskId).orElseThrow(() -> new IllegalArgumentException("Task not found"));
-        this.schedulerService.scheduleTask(task);
-
-    }
-
-    public void cancelTask(Long taskId) {
-        Task task = this.taskRepository.findById(taskId).orElseThrow(() -> new IllegalArgumentException("Task not found"));
-        this.schedulerService.cancelTask(task);
-    }
-
-    public List<Task> findAll() {
-        return this.taskRepository.findAll();
+    public List<TaskDto> findAll() {
+        return List.of(new TaskDto("nom", "cron"));
     }
 }
